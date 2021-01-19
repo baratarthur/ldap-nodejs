764c9735fc50de83d2e61c2850997e53be151855
Avivaldo Ribeiro <avivaldo.ribeiro@gmail.com>
2021-01-19 19:15:41 -0300

    initial user create

--- controllers/index.js
+++ controllers/index.js
@@ -7,33 +7,84 @@ const routes = new Router();
 routes.get('/', (req, res) => {
   const {user, password} = req.body
   const ldap = new Ldap(user, password)
-  if (!ldap) {
-    res.status(400).send({Error: 'Bind error!'})
+  if (!user||!password) {
+    res.status(401).send({Error: 'Campo obrigatório não informado!'})
   }else{
     res.status(200).json(ldap.client.urls)
   }
 })
 
-routes.get('/busca', async (req, res) => {
+routes.get('/usuarios', async (req, res) => {
   try {
-    const ldap = new Ldap('avivaldo', '123')
-    const options = {
-      attributes: ['dn', 'cn', 'description', 'memberOf', 'employeeNumber', 'employeeID', 'otherMailbox', 'mobile'],
-      scope: 'sub',
-      filter: '(givenName=*)'
-    };
-    
-    const busca = await ldap.search('OU=DCC,OU=USERS,OU=PEOPLE,DC=dccpm, DC=dev', options)
-    
-    if (!ldap&&!busca) {
-      res.status(400).send({Error: 'Bind and search error!'}) 
+    const { user, password, userSearch } = req.body;
+    if (!user||!password) {
+      res.status(401).send({Error: 'Campo obrigatório não informado!'})
     } else {
-      res.status(200).json(busca)
+      const ldap = new Ldap(user, password);
+      const options = {
+        attributes: ['dn', 'cn', 'description', 'memberOf', 'employeeNumber', 'employeeID', 'otherMailbox', 'mobile'],
+        scope: 'sub',
+        filter: `(givenName=${userSearch})`
+      };
+      
+      const busca = await ldap.search(`OU=USERS,OU=PEOPLE,DC=dccpm, DC=dev`, options)
+      console.log(busca)
+      if (!ldap&&!busca) {
+        res.status(400).send({Error: 'Bind and search error!'}) 
+      } else if (busca=='') {
+        res.status(200).json({Resultado: 'Nenhum usuário localizado!'})
+      } else {
+        res.status(200).json(busca)
+      }
     }
   } catch (error) {
     console.log(error)  
   }
-  
+})
+
+routes.post('/usuarios', async (req, res) => {
+  try {
+    const { user, password, dn, entry } = req.body;  
+    if (!dn||!entry) {
+      res.status(400).json({Error: 'Parâmetro obrigatório não informado!'})
+    } else {
+      const ldap = await new Ldap(user, password)
+      const criar = await ldap.add(dn, entry)
+      console.log(`Criar: ${criar}`)
+      res.status(201).json(criar)
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error)
+  }
+})
+
+routes.get('/grupos', async (req, res) => {
+  try {
+    const { user, password, group } = req.body;
+    if (!user||!password||!group) {
+      res.status(401).send({Error: 'Campo obrigatório não informado!'})
+    } else {
+      const ldap = new Ldap(user, password);
+      const options = {
+        attributes: ['member'],
+        scope: 'sub',
+        filter: `(CN=${group})`
+      };
+      
+      const busca = await ldap.search(`OU=GROUPS,OU=PEOPLE,DC=dccpm, DC=dev`, options)
+      console.log(busca)
+      if (!ldap&&!busca) {
+        res.status(400).send({Error: 'Bind and search error!'}) 
+      } else if (busca=='') {
+        res.status(200).json({Resultado: 'Nenhum grupo localizado!'})
+      } else {
+        res.status(200).json(busca)
+      }
+    }
+  } catch (error) {
+    console.log(error)  
+  }
 })
 
 module.exports = routes;
--- models/Ldap.js
+++ models/Ldap.js
@@ -9,11 +9,33 @@ class Ldap {
 
   bind(dn, password) {
     return new Promise((resolve, reject) => {
-      const bind = this.client.bind(dn, password, (err) => {
+      const bind = this.client.bind(dn, password, err => {
         try {
-         resolve(bind)
+          if (err) {
+            console.log(`Error: ${err}`)
+          } else {
+            resolve(bind)
+          }
         }
         catch (error) {
+          reject(error)
+        }
+      })
+    })
+  }
+
+  async add(dn, entry) {
+    return await new Promise((resolve, reject) => {
+      const addUser = this.client.add(dn, entry, err => {
+        try {
+          if (err) {
+            console.log(`Error: ${err}`)
+          } else {
+            console.log(addUser)
+            resolve(addUser)
+          }
+        } catch (error) {
+          console.log(error)
           reject(error)
         }
       })
@@ -25,8 +47,9 @@ class Ldap {
       this.client.search(base, options, (err, res) => {
         try {
           const entries = [];
-          res.on('searchEntry', function(entry) {
-            entries.push(entry);
+          res.on('searchEntry', async function(entry) {
+            const memberOf = await JSON.parse(entry);
+            entries.push(memberOf);
           });
           res.on('searchReference', function(referral) {
             console.log('referral: ' + referral.uris.join());
@@ -39,6 +62,7 @@ class Ldap {
             if (result.status !== 0) {
               reject(new Error('Error code received from Active Directory'));
             } else {
+              console.log(entries)
               resolve(entries)
             }
           });
