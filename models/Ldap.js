9746fe01cdb1913b2e1d4a5d0d274f930aa4cf6d
Arthur Barata <arthurpbarata@gmail.com>
2021-01-22 22:01:55 -0300

    melhorar codigo model
    tentativa 1

--- models/Ldap.js
+++ models/Ldap.js
@@ -4,86 +4,73 @@ class Ldap {
 
   constructor(dn, password) {
     this.client = client;
-    this.bind(dn, password);
+    this.dn = dn;
+    this.password = password;
   }
 
   bind(dn, password) {
     return new Promise((resolve, reject) => {
-      const bind = this.client.bind(dn, password, err => {
-        try {
-          if (err) {
-            console.log(`Error: ${err}`)
-          } else {
-            resolve(bind)
-          }
-        }
-        catch (error) {
-          reject(error)
-        }
+      this.client.bind(dn, password, (err, response) => {
+        if (err) reject(err)
+        else resolve(response)
       })
     })
   }
 
-  async add(dn, entry) {
-    return await new Promise((resolve, reject) => {
-      const addUser = this.client.add(dn, entry, (err) => {
-        try {
-          if (err) {
-            console.log(`Error create user: ${err}`)
-          } else {
-            resolve(addUser)
-          }
-        } catch (error) {
-          reject(error)
+  add(dn, entry) {
+    return new Promise((resolve, reject) => {
+      this.bind(this.dn, this.password).then(
+        info => {
+          this.client.add(dn, entry, (err, res) => {
+            if(err) reject(err)
+            else resolve(res)
+          });
         }
-      })
+      ).catch(err => reject(err));
     })
   }
 
   del(dn) {
     return new Promise((resolve, reject) => {
-      const delUser = this.client.del(dn, (err) => {
-        try {
-          if (err) {
-            console.log(`Error delete user: ${err}`)
-          } else {
-            resolve(delUser)
-          }
-        } catch (error) {
-          reject(error)
+       this.bind(this.dn, this.password).then(
+        info => {
+            this.client.del(dn, (err, res) => {
+                if (err) reject(err)
+                else resolve(res)
+            })
         }
-      })
+      ).catch(err => reject(err));
     })
   }
 
-  async search(base, options) {
-    return await new Promise((resolve, reject) => {
-      this.client.search(base, options, (err, res) => {
-        try {
-          const entries = [];
-          res.on('searchEntry', (entry) => {
-            const data = entry.toString()
-            entries.push(data);
-          });
-          res.on('searchReference', function(referral) {
-            console.log('referral: ' + referral.uris.join());
-          });
-          res.on('error', function(err) {
-            console.error('error: ' + err.message);
-          });
-          res.on('end', function(result) {
-            console.log('status: ' + result.status);
-            if (result.status !== 0) {
-              reject(new Error('Error code received from Active Directory'));
-            } else {
-              console.log(entries)
-              resolve(entries)
-            }
-          });
-        } catch (error) {
-          reject(error)
+  search(base, options) {
+    return new Promise((resolve, reject) => {
+      this.bind(this.dn, this.password).then(
+        info => {
+            this.client.search(base, options, (err, res) => {
+                const entries = [];
+                res.on('searchEntry', (entry) => {
+                  const data = entry.toString()
+                  entries.push(data);
+                });
+                res.on('searchReference', function(referral) {
+                  console.log('referral: ' + referral.uris.join());
+                });
+                res.on('error', function(err) {
+                  console.error('error: ' + err.message);
+                });
+                res.on('end', function(result) {
+                  console.log('status: ' + result.status);
+                  if (result.status !== 0) {
+                    reject(new Error('Error code received from Active Directory'));
+                  } else {
+                    console.log(entries)
+                    resolve(entries)
+                  }
+                });
+            })
         }
-      })
+      ).catch(err => reject(err));
     })
   }
 }
